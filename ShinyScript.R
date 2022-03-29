e3c6b650bad9b6cb79efa43b7dd5e4550d754546
Dan Hodgin <danhodgin@MacBook-Air-2.lan>
2022-03-29 19:39:42 +0100

    configuring the y axis label

--- ShinyScript.R
+++ ShinyScript.R
@@ -20,74 +20,11 @@ mutation.dat <- readMAF(mutation.csv,
                         sep = ",",
                         quote = "\"")  # column-separator of csv file
 
-geneSelected <- "TP53"
+
+
+geneSelected <- "TP53" 
 
 # set up chart options
-plot.options <- g3Lollipop.options(
-  # Chart settings
-  chart.width = 900,
-  chart.type = "pie",
-  chart.margin = list(left = 30, right = 20, top = 20, bottom = 30),
-  chart.background = "#d3d3d3",
-  transition.time = 300,
-  # Lollipop track settings
-  lollipop.track.height = 200,
-  lollipop.track.background = "#d3d3d3",
-  lollipop.pop.min.size = 1,
-  lollipop.pop.max.size = 8,
-  lollipop.pop.info.limit = 5.5,
-  lollipop.pop.info.dy = "0.24em",
-  lollipop.pop.info.color = "white",
-  lollipop.line.color = "#a9A9A9",
-  lollipop.line.width = 3,
-  lollipop.circle.color = "#ffdead",
-  lollipop.circle.width = 0.4,
-  lollipop.label.ratio = 2,
-  lollipop.label.min.font.size = 12,
-  lollipop.color.scheme = "dark2",
-  highlight.text.angle = 60,
-  # Domain annotation track settings
-  anno.height = 16,
-  anno.margin = list(top = 0, bottom = 0),
-  anno.background = "#d3d3d3",
-  anno.bar.fill = "#a9a9a9",
-  anno.bar.margin = list(top = 4, bottom = 4),
-  domain.color.scheme = "pie5",
-  domain.margin = list(top = 2, bottom = 2),
-  domain.text.color = "white",
-  domain.text.font = "italic 8px Serif",
-  # Y-axis label
-  y.axis.label = paste("# of ", geneSelected, "gene mutations"),
-  axis.label.color = "#303030",
-  axis.label.alignment = "end",
-  axis.label.font = "italic 12px Serif",
-  axis.label.dy = "-1.5em",
-  y.axis.line.color = "#303030",
-  y.axis.line.width = 0.5,
-  y.axis.line.style = "line",
-  y.max.range.ratio = 1.1,
-  # Chart title settings
-  title.color = "#303030",
-  title.text = paste(geneSelected, "gene"),
-  title.font = "bold 12px monospace",
-  title.alignment = "start",
-  # Chart legend settings
-  legend = TRUE,
-  legend.margin = list(left=20, right = 0, top = 10, bottom = 5),
-  legend.interactive = TRUE,
-  legend.title = "Variant classification",
-  # Brush selection tool
-  brush = TRUE,
-  brush.selection.background = "#F8F8FF",
-  brush.selection.opacity = 0.3,
-  brush.border.color = "#a9a9a9",
-  brush.border.width = 1,
-  brush.handler.color = "#303030",
-  # tooltip and zoom
-  tooltip = TRUE,
-  zoom = TRUE
-)
-
 
 
 
@@ -108,21 +45,116 @@ shinyApp(
  )
  ),
   server = function(input, output) {
-    geneSelected <- reactive(input$gene)
+    geneSelected <- reactive(as.character(input$gene)) 
+
+    # try if the gene selected is null
+    
+    #   if (!is.null(reactive(input$gene))) {
+    #   newGene <- reactive(input$gene)
+    # } else {
+    #   newGene <- reactive("Hello")
+    # }
+      
+    
+    
+    
+    
+    
+      #print(input$gene)
+      #print(plot.options$yAxisLabel)
+  
+    
+    
+    #reactiveVal("TP53")
      # input$gene <- 
+    #observeEvent(input$gene, {
+      #geneSelected <- geneSelected()
+      
+      
+    #})
+    
+    #plot.options.g3Lollipop.options.y.axis.label <- paste("# of ", geneSelected(), "gene mutations")
+    
+    
     
     
     
     output$lolliplot <- renderG3Lollipop({
+      
         g3Lollipop(mutation.dat,
                    # gene symbol here can be chosen from a dropdown menu
                    gene.symbol = geneSelected(),
                    protein.change.col = "amino_acid_change",
                    btn.style = "blue", # blue-style chart download buttons
-                   plot.options = plot.options,
+                   plot.options = g3Lollipop.options(
+                     # Chart settings
+                     chart.width = 900,
+                     chart.type = "pie",
+                     chart.margin = list(left = 30, right = 20, top = 20, bottom = 30),
+                     chart.background = "#d3d3d3",
+                     transition.time = 300,
+                     # Lollipop track settings
+                     lollipop.track.height = 200,
+                     lollipop.track.background = "#d3d3d3",
+                     lollipop.pop.min.size = 1,
+                     lollipop.pop.max.size = 8,
+                     lollipop.pop.info.limit = 5.5,
+                     lollipop.pop.info.dy = "0.24em",
+                     lollipop.pop.info.color = "white",
+                     lollipop.line.color = "#a9A9A9",
+                     lollipop.line.width = 3,
+                     lollipop.circle.color = "#ffdead",
+                     lollipop.circle.width = 0.4,
+                     lollipop.label.ratio = 2,
+                     lollipop.label.min.font.size = 12,
+                     lollipop.color.scheme = "dark2",
+                     highlight.text.angle = 60,
+                     # Domain annotation track settings
+                     anno.height = 16,
+                     anno.margin = list(top = 0, bottom = 0),
+                     anno.background = "#d3d3d3",
+                     anno.bar.fill = "#a9a9a9",
+                     anno.bar.margin = list(top = 4, bottom = 4),
+                     domain.color.scheme = "pie5",
+                     domain.margin = list(top = 2, bottom = 2),
+                     domain.text.color = "white",
+                     domain.text.font = "italic 8px Serif",
+                     # Y-axis label
+                     y.axis.label = paste("Number of ", geneSelected(), "mutations"),
+                     axis.label.color = "#303030",
+                     axis.label.alignment = "end",
+                     axis.label.font = "italic 12px Serif",
+                     axis.label.dy = "-1.5em",
+                     y.axis.line.color = "#303030",
+                     y.axis.line.width = 0.5,
+                     y.axis.line.style = "line",
+                     y.max.range.ratio = 1.1,
+                     # Chart title settings
+                     title.color = "#303030",
+                     title.text = paste(geneSelected(), "gene"),
+                     title.font = "bold 12px monospace",
+                     title.alignment = "start",
+                     # Chart legend settings
+                     legend = TRUE,
+                     legend.margin = list(left=20, right = 0, top = 10, bottom = 5),
+                     legend.interactive = TRUE,
+                     legend.title = "Variant classification",
+                     # Brush selection tool
+                     brush = TRUE,
+                     brush.selection.background = "#F8F8FF",
+                     brush.selection.opacity = 0.3,
+                     brush.border.color = "#a9a9a9",
+                     brush.border.width = 1,
+                     brush.handler.color = "#303030",
+                     # tooltip and zoom
+                     tooltip = TRUE,
+                     zoom = TRUE
+                   ),
                    output.filename = "customized_plot")
       })
     
   }
 
 )
+
+
