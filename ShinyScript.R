fae00c085937a4a5579a5f15b2839e5cd6f0b80e
Dan Hodgin <85701500+hodgln@users.noreply.github.com>
2022-05-23 10:54:25 +0100

    Add files via upload

--- ShinyScript.R
+++ ShinyScript.R
@@ -2,21 +2,32 @@
 
 # server <- 
 library(shiny)
-library(g3viz)
+library(g3vizModified)
 library(glue)
+library(httr)
+library(jsonlite)
+library(tidyverse)
+library(purrr)
 
 # change the file path to your own
-geneSymbols <- data.frame((read.csv("/Users/danhodgin/Documents/University/Intercalation/annotation/mutationsEdit.csv"))["Hugo_Symbol"])
+geneSymbols <- read.csv("/Users/danhodgin/Documents/University/Intercalation/annotation/allData.csv")["Hugo_Symbol"]
 
-listGenes <- unique(geneSymbols)
+listGenes <- unique(geneSymbols$Hugo_Symbol)
+
+index <- which(listGenes == ".")
+
+listGenes <- listGenes[-index]
+
 
 # change the file path to your own
-mutation.csv <- "/Users/danhodgin/Documents/University/Intercalation/annotation/mutationsEdit.csv"
+mutation.csv <- "/Users/danhodgin/Documents/University/Intercalation/annotation/allData.csv"
 
 mutation.dat <- readMAF(mutation.csv,
                         gene.symbol.col = "Hugo_Symbol",
                         variant.class.col = "Variant_Classification",
                         protein.change.col = "amino_acid_change",
+                        start.pos.col = "Start_Position",
+                        chr.col = "Chromosome",
                         sep = ",",
                         quote = "\"")  # column-separator of csv file
 
@@ -46,61 +57,68 @@ shinyApp(
  ),
   server = function(input, output) {
     geneSelected <- reactive(as.character(input$gene)) 
-
-    # try if the gene selected is null
-    
-    #   if (!is.null(reactive(input$gene))) {
-    #   newGene <- reactive(input$gene)
-    # } else {
-    #   newGene <- reactive("Hello")
-    # }
-      
-    
-    
-    
-    
-    
-      #print(input$gene)
-      #print(plot.options$yAxisLabel)
-  
-    
-    
-    #reactiveVal("TP53")
-     # input$gene <- 
-    #observeEvent(input$gene, {
-      #geneSelected <- geneSelected()
-      
-      
-    #})
-    
-    #plot.options.g3Lollipop.options.y.axis.label <- paste("# of ", geneSelected(), "gene mutations")
-    
-    
     
     
     
     output$lolliplot <- renderG3Lollipop({
       
+        server <- "https://rest.ensembl.org"
+        ext <- "/lookup/symbol/homo_sapiens/"
+        
+      
+        r <- GET(paste(server, ext, geneSelected(), "?expand=1", sep = ""), content_type("application/json"))
+        
+
+        parsedData <- head(data.frame(t(sapply(content(r),c))))
+        
+        
+        dataone <- parsedData$Transcript$Transcript
+        
+        exons <- sapply(dataone, function(i) i["Exon"])
+        
+        
+        newExons <- unique(exons)
+        
+        
+        listExons <- purrr::flatten(newExons)
+    
+        
+        end <- (lapply(listExons, `[[`, "end"))
+        start <- (lapply(listExons, `[[`, "start"))
+        name <- (lapply(listExons, `[[`, "object_type"))
+        
+        transcripts <- data.frame(cbind(end, start, name))
+        
+        #select chromosome using gene. 
+        geneData <- subset(mutation.dat, Hugo_Symbol == geneSelected())
+        
+        #startAndStop <- parsedData$gene
+      
         g3Lollipop(mutation.dat,
                    # gene symbol here can be chosen from a dropdown menu
                    gene.symbol = geneSelected(),
                    protein.change.col = "amino_acid_change",
+                   start.pos.col = "Start_Position",
+                   chr.col = "Chromosome",
+                   transcript.values = transcripts,
+                   gene.pos.values = c(parsedData$start$start, parsedData$end$end),
+                   chr.value = geneData$Chromosome[1],
                    btn.style = "blue", # blue-style chart download buttons
                    plot.options = g3Lollipop.options(
                      # Chart settings
                      chart.width = 900,
                      chart.type = "pie",
                      chart.margin = list(left = 30, right = 20, top = 20, bottom = 30),
-                     chart.background = "#d3d3d3",
+                     chart.background = "white",
                      transition.time = 300,
                      # Lollipop track settings
                      lollipop.track.height = 200,
-                     lollipop.track.background = "#d3d3d3",
+                     lollipop.track.background = "white",
                      lollipop.pop.min.size = 1,
                      lollipop.pop.max.size = 8,
                      lollipop.pop.info.limit = 5.5,
                      lollipop.pop.info.dy = "0.24em",
-                     lollipop.pop.info.color = "white",
+                     lollipop.pop.info.color = "#d3d3d3",
                      lollipop.line.color = "#a9A9A9",
                      lollipop.line.width = 3,
                      lollipop.circle.color = "#ffdead",
@@ -112,12 +130,12 @@ shinyApp(
                      # Domain annotation track settings
                      anno.height = 16,
                      anno.margin = list(top = 0, bottom = 0),
-                     anno.background = "#d3d3d3",
+                     anno.background = "white",
                      anno.bar.fill = "#a9a9a9",
                      anno.bar.margin = list(top = 4, bottom = 4),
                      domain.color.scheme = "pie5",
                      domain.margin = list(top = 2, bottom = 2),
-                     domain.text.color = "white",
+                     domain.text.color = "#d3d3d3",
                      domain.text.font = "italic 8px Serif",
                      # Y-axis label
                      y.axis.label = paste("Number of ", geneSelected(), "mutations"),
